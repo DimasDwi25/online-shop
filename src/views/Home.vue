21d02d866d0fad5e2a27225122d596e9b4bd0da0
DimasDwi25 <ddwi673@gmail.com>
2020-11-08 19:38:07 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerLogo />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerLogo,
+    FooterShayna
+    
+  },
+};
+</script>
